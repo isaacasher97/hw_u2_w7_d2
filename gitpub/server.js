591ea5cf2d7a877667e432f4144fbbcbc6b66ef7
isaacasher97 <isaac.asher97@gmail.com>
2023-06-07 22:29:18 -0400

    setting up database section completed

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,11 +1,13 @@
+//Dependencies and setup
 require("dotenv").config() // loads the variables in the .env into the process.env object
 const express = require("express") //require express
 const morgan = require("morgan")
+const drinks = require("./models/drinks") // return whatever drinks.js exports
 const PORT = process.env.PORT // getting the port from the .env file
 const app = express() // see express() to a variable
 
 
-
+//Middleware
 app.use(morgan("dev")) // Middleware
 
 
@@ -15,6 +17,9 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 
 
 
